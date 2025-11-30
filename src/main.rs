25b241a0185498e536cfd2cc6f51cc23a1cdf1a2
marty777 <martin.thorne@gmail.com>
2025-11-30 09:47:07 -0700

    Initial setup

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+pub mod util;
+
+use clap::{Arg, Command};
+use std::time::{Instant};
+
+const MAXDAY:usize = 0;
+
+fn main() {
+    
+    let command = Command::new("adventofcode2025").max_term_width(80)
+					.about("Advent of Code 2025 solutions")
+                    .arg(Arg::new("day").help("Puzzle day to run").short('d').long("day").value_name("DAY").required(true))
+                    .arg(Arg::new("input").help("Path to input file").short('i').long("input").value_name("PATH"));
+	let args = command.get_matches();	
+	let day:usize;
+    match args.get_one::<String>("day") {
+        Some(day_arg) => {
+            match day_arg.parse::<usize>() {
+                Ok(n) => {
+                    day = n;
+                    if day < 1 || day > MAXDAY {
+                        println!("DAY must be in range 1 - {} ({} provided)", MAXDAY, n);
+                        std::process::exit(2);
+                    }
+                },
+                Err(_) => {
+                    println!("Could not parse DAY argument '{}' as an integer.", day_arg);
+                    std::process::exit(2);
+                }
+            }
+        },
+        None => {
+            println!("Day argument is required");
+            std::process::exit(2);
+        }
+    }
+    let input_path:String;
+    match args.get_one::<String>("input") {
+        Some(input_arg) => {
+            input_path = input_arg.clone();
+        },
+        None => {
+            input_path = format!("./data/day{}/input.txt", day);
+        }
+    }
+    
+    println!(r#"   ___     __              __         ___  _____        __      _  ___  ____
+  / _ |___/ /  _____ ___  / /_  ___  / _/ / ___/__  ___/ /__   ( )|_  |/ __/
+ / __ / _  / |/ / -_) _ \/ __/ / _ \/ _/ / /__/ _ \/ _  / -_)  |// __//__ \ 
+/_/ |_\_,_/|___/\__/_//_/\__/  \___/_/   \___/\___/\_,_/\__/    /____/____/ "#);
+    
+    println!("\nDay:\t{}\nInput:\t{}", day, input_path);
+            
+    match util::read_input(&input_path) {
+        Err(e) => {
+            println!("Error reading input: {}", e);
+            std::process::exit(2);
+        },
+        Ok(input_lines) => {
+            let start_instant = Instant::now();
+            match day {
+                _ => { println!("Day {} not matched", day)}
+            }
+            println!("\nTotal execution time: {:?}", start_instant.elapsed());
+        }
+    }
+}
---
+++ src/util.rs
@@ -0,0 +1,385 @@
+#[allow(dead_code)]
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use regex::Regex;
+
+// General helper functions
+
+// File input
+/// Read an input file into lines
+pub fn read_input(file_path:&str) -> Result<Vec<String>, String>{
+    match File::open(file_path) {
+        Err(_) => Err(format!("Unable to open file at path {}", file_path)),
+        Ok(file) => {
+            let mut vec:Vec<String> = Vec::new();
+            let reader = BufReader::new(file);
+            for (_index, line) in reader.lines().enumerate() {
+                let line = line.unwrap(); // Ignore errors.
+                vec.push(line);
+            }
+	        return Ok(vec);
+        }
+    }
+}
+/// Break input lines into sections on empty lines
+pub fn sections(lines:&Vec<String>) -> Vec<Vec<String>> {
+    let mut sections:Vec<Vec<String>> = Vec::new();
+    sections.push(Vec::new());
+    let mut section_count = 1;
+    for i in 0..lines.len() {
+        if lines[i].len() == 0 {
+            sections.push(Vec::new());
+            section_count += 1;
+        }
+        else {
+            sections[section_count - 1].push(lines[i].clone());
+        }
+    }
+    return sections;
+}
+
+/// Extract all base 10 integers in a string
+pub fn ints_in_string(string:&String) -> Vec<isize> {
+    let re = Regex::new(r"-?\d+\.?\d*").unwrap();
+    let string_matches: Vec<&str> = re.find_iter(string).map(|m| m.as_str()).collect();
+    let mut result:Vec<isize> = Vec::new();
+    for s in string_matches {
+        match s.parse::<isize>() {
+            Ok(x) => result.push(x),
+            _ => {}
+        }
+    }
+    return result;
+}
+
+/// Extract all base 10 floats in a string (integers will be parsed as floats)
+pub fn floats_in_string(string:&String) -> Vec<f64> {
+    let re = Regex::new(r"-?\d+\.?\d*").unwrap();
+    let string_matches: Vec<&str> = re.find_iter(string).map(|m| m.as_str()).collect();
+    let mut result:Vec<f64> = Vec::new();
+    for s in string_matches {
+        match s.parse::<f64>() {
+            Ok(x) => result.push(x),
+            _ => {}
+        }
+    }
+    return result;
+}
+
+/// Default dict equivalent with keys of type `T` and values of type `U` 
+pub struct DefaultHashMap<T,U> {
+    map:HashMap<T,U>,
+    default:U
+}
+impl<T: Eq + Hash,U> DefaultHashMap<T,U> {
+    pub fn new(default:U) -> DefaultHashMap<T,U> {
+        return DefaultHashMap { map:HashMap::<T,U>::new(), default };
+    }
+    pub fn insert(&mut self, key:T, val:U) -> Option<U> {
+        return self.map.insert(key, val);
+    }
+    pub fn len(&self) -> usize {
+        return self.map.len();
+    }
+    pub fn get(&self, key:&T) -> &U {
+        if self.map.contains_key(key) {
+            return self.map.get(key).unwrap();
+        }
+        else {
+            return &self.default;
+        }
+    }
+    pub fn get_mut(&mut self, key:&T) -> Option<&mut U> {
+        if self.map.contains_key(key) {
+            return Some(self.map.get_mut(key).unwrap());
+        }
+        else {
+            return None;
+        }
+    }
+}
+
+// 2D, 3D vectors
+
+/// 2D Vector struct
+#[derive(Eq, PartialEq, Hash)]
+pub struct Vec2 {
+    pub x:isize,
+    pub y:isize
+}
+impl Vec2 {
+    pub fn new(x:isize, y:isize) -> Vec2 {
+        return Vec2{x:x, y:y};
+    }
+    pub fn newu(x:usize, y:usize) -> Vec2 {
+        return Vec2{x:x as isize, y:y as isize};
+    }
+    /// Tests if the coordinate is within the bounds of a zero-based rectange with dimensions `dim_x`, `dim_y`.
+    pub fn in_bounds(self, dim_x:usize, dim_y:usize) -> bool {
+        return self.x >= 0 && self.x < dim_x as isize && self.y >= 0 && self.y < dim_y as isize;
+    }
+}
+impl std::fmt::Display for Vec2 {
+    fn fmt(&self, f: &mut  std::fmt::Formatter) ->  std::fmt::Result {
+		write!(f,"({},{})", self.x, self.y)
+    }
+}
+impl std::ops::Add<Vec2> for Vec2 {
+    type Output = Vec2;
+    fn add(self, rhs:Vec2) -> Vec2 {
+        return Vec2{x:self.x + rhs.x, y:self.y + rhs.y};
+    }
+}
+impl std::ops::Mul<isize> for Vec2 {
+    type Output = Vec2;
+    fn mul(self, rhs:isize) -> Vec2 {
+        return Vec2{x:self.x * rhs, y:self.y * rhs};
+    }
+}
+/// 3D vector struct
+pub struct Vec3 {
+    pub x:isize,
+    pub y:isize,
+    pub z:isize
+}
+impl Vec3 {
+    pub fn new(x:isize, y:isize, z:isize) -> Vec3 {
+        return Vec3{x:x, y:y, z:z};
+    }
+    pub fn newu(x:usize, y:usize, z:usize) -> Vec3 {
+        return Vec3{x:x as isize, y:y as isize, z:z as isize};
+    }
+    /// Tests if the coordinate is within the bounds of a zero-based rectange with dimensions `dim_x`, `dim_y`, `dim_z`.
+    pub fn in_bounds(self, dim_x:usize, dim_y:usize, dim_z:usize) -> bool {
+        return self.x >= 0 && self.x < dim_x as isize && self.y >= 0 && self.y < dim_y as isize && self.z >= 0 && self.z < dim_z as isize;
+    }
+}
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut  std::fmt::Formatter) ->  std::fmt::Result {
+		write!(f,"({},{},{})", self.x, self.y, self.z)
+    }
+}
+impl std::ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs:Vec3) -> Vec3 {
+        return Vec3{x:self.x + rhs.x, y:self.y + rhs.y, z:self.z + rhs.z};
+    }
+}
+impl std::ops::Mul<isize> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs:isize) -> Vec3 {
+        return Vec3{x:self.x * rhs, y:self.y * rhs, z:self.z * rhs};
+    }
+}
+
+// 2D grid functions
+
+/// Reads a grid as a 2D vector of `char`s
+pub fn read_grid(lines:&Vec<String>) -> Vec<Vec<char>> {
+    let mut grid:Vec<Vec<char>> = Vec::new();
+    for i in 0..lines.len() {
+        grid.push(Vec::new());
+        for c in lines[i].chars() {
+            grid[i].push(c);
+        }
+    }
+    return grid;
+}
+
+/// Returns the grid as a DefaultHashMap, plus the grid width and height
+pub fn read_grid_map(lines:&Vec<String>, default_char:char) -> Result<(DefaultHashMap<Vec2, char>, usize, usize), String> {
+    let mut map = DefaultHashMap::<Vec2, char>::new(default_char);
+    if lines.len() == 0 {
+        return Err("lines are empty".to_string());
+    }
+    let width = lines[0].len();
+    let height = lines.len();
+    for y in 0..lines.len() {
+        if lines[y].len() != width {
+            return Err(format!("Irregular grid: expecting width {} at line {}, found {}", width, y, lines[y].len()));
+        }
+        let line_chars:Vec<char> = lines[y].chars().collect();
+        for x in 0..width {
+            map.insert(Vec2::newu(x,y), line_chars[x]);
+        }
+    }
+    return Ok((map, width, height));
+}
+
+/// Delta vectors to orthogonally adjacent coords N,E,S,W
+pub fn adjacent4() -> Vec<Vec2> {
+    return vec![Vec2::new(0,-1), Vec2::new(1,0), Vec2::new(0,1), Vec2::new(-1,0)];
+}
+/// Delta vectors to orthogonally adjacent coords N,E,S,W + self
+pub fn adjacent5() -> Vec<Vec2> {
+    return vec![Vec2::new(0,-1), Vec2::new(1,0), Vec2::new(0,1), Vec2::new(-1,0), Vec2::new(0,0)];
+}
+/// Delta vectors to adjacent coords including diagonals N,NE,E,SE,S,SW,W,NW
+pub fn adjacent8() -> Vec<Vec2> {
+    return vec![Vec2::new(0,-1), Vec2::new(1,-1), Vec2::new(1,0), Vec2::new(1,1), Vec2::new(0,1), Vec2::new(-1,1), Vec2::new(-1,0), Vec2::new(-1,-1)];
+}
+/// Delta vectors to adjacent coords including diagonals N,NE,E,SE,S,SW,W,NW + self
+pub fn adjacent9() -> Vec<Vec2> {
+    return vec![Vec2::new(0,-1), Vec2::new(1,-1), Vec2::new(1,0), Vec2::new(1,1), Vec2::new(0,1), Vec2::new(-1,1), Vec2::new(-1,0), Vec2::new(-1,-1), Vec2::new(0,0)];
+}
+/// Return HashMap of directional characters `^>v<` to direction deltas
+pub fn arrow_dirs() -> HashMap<char, Vec2> {
+    return HashMap::<char, Vec2>::from([('^', Vec2::new(0,-1)), ('>', Vec2::new(1,0)), ('v', Vec2::new(0,1)), ('<', Vec2::new(-1,0))]);
+}
+
+// Math - using i128s for most parameters. May revise if it causes performance difficulties.
+
+/// Basic Sieve of Eratosthenes. Returns all integers less than `limit` and 
+/// greater than 1 that are prime (more or less, no extraordinary measures 
+/// regarding scale)
+pub fn eratosthenes(limit:i128) -> Vec<i128> { 
+    let mut local_limit = limit;
+    if local_limit < 2 {
+        local_limit = 2;
+    }
+    let mut primes = Vec::new();
+    let mut sieve = vec![false; local_limit as usize];
+    let mut increment = 2;
+    loop {
+        for i in (2*increment..local_limit).step_by(increment as usize) {
+            sieve[i as usize] = true;
+        }
+        let mut done = true;
+        for i in (increment+1)..local_limit {
+            if !sieve[i as usize] {
+                increment = i;
+                done = false;
+                break;
+            }
+        }
+        if done {
+            break;
+        }
+    }
+    for i in 2..local_limit {
+        if !sieve[i as usize] {
+            primes.push(i);
+        }
+    }
+    return primes;
+}
+/// Return the digits of `x` in base `n` read left to right, optionally padded to `required_len`
+pub fn base_n_digits(x:i128, n:usize, required_len:Option<usize>) -> Vec<i128> {
+    let mut digits = Vec::new();
+    let mut x_temp = x;
+    while x_temp > 0 {
+        // prepend next most significant digit
+        digits.insert(0, x_temp % (n as i128));
+        x_temp -= x % (n as i128);
+        x_temp /= n as i128;
+    }
+    match required_len {
+        Some(len) => {
+            // prepend with zeroes
+            while digits.len() < len {
+                digits.insert(0, 0);
+            }
+        },
+        None => {}
+    }
+    return digits;
+}
+/// Compute the GCD of `a` and `b`
+pub fn gcd(a:i128, b:i128) -> i128 {
+    if a == b { 
+        return a; 
+    }
+    let mut a_local = a;
+    let mut b_local = b;
+    if b > a {
+        a_local = b;
+        b_local = a;
+    }
+    while b_local > 0 {
+        let temp = a_local;
+        a_local = b_local;
+        b_local = temp % b_local;
+    }
+    return a_local;
+}
+/// Compute  (GCD(`a`,`b`), `x`, `y`) such that `ax` + `by` = GCD(`a`,`b`)
+/// via the extended Euclidean algorithm
+pub fn extended_gcd(a:i128, b:i128) -> (i128, i128, i128) {
+    if a == 0 {
+        return (b, 0, 1);
+    }
+    else {
+        let (gcd, x, y) = extended_gcd(b % a, a);
+        return (gcd, y - (b/a) * x, x);
+    }
+}
+/// Construct `x`^`n` % `p`
+pub fn mod_exp(x:i128 , n:i128 , m:i128) -> i128{
+    if n <= 0 { 
+        return 1; 
+    }
+    let mut result = 1;
+    let mut n_local = n;
+    let mut x_local = x;
+    loop {
+        if n_local == 1 { 
+            return (result * x_local) % m; 
+        }
+        // if n even, square x, halve n
+        if n_local % 2 == 0 {
+            x_local = (x_local * x_local ) % m;
+            n_local /= 2;
+        }
+        // if n odd, mult result by x, decrement n
+        else {
+            result = (result * x_local) % m;
+            n_local -= 1;
+        }
+    }
+}
+/// Compute the modular multiplicative inverse of `x` modulo `p` if `p` >= 1
+/// and `x`,`p` coprime
+pub fn mod_inv(x:i128, p:i128) -> Option<i128> {
+    if p < 1 {
+        return None;
+    }
+    let (gcd, i, _) = extended_gcd(x, p);
+    if gcd != 1 {
+        return None;
+    }
+    return Some((i % p + p) % p);
+}
+/// Compute the least common multiple of `a`,`b`
+pub fn lcm(a:i128, b:i128) -> i128 {
+    return (a*b).abs() / gcd(a, b);
+}
+/// Calculate a remainder satisfying all given `congruences` (having pairwise 
+/// coprime moduli) using the Chinese Remainder Theorem. Congruences are passed
+/// as a tuples of (modulus, remainder)
+pub fn crt(congruences:&Vec<(i128, i128)>) -> Option<i128> {
+    // require all moduli to be pairwise coprime
+    for i in 0..congruences.len() {
+        for j in i+1..congruences.len() {
+            if gcd(congruences[i].0, congruences[j].0) != 1 {return None};
+        }
+    }
+    let mut sum = 0;
+    let mut product = 1;
+    for i in 0..congruences.len() {
+        product *= congruences[i].0;
+    }
+    for i in 0..congruences.len() {
+        let m = product / congruences[i].0;
+        match mod_inv(m, congruences[i].0) {
+            Some(inv) => {
+                sum += congruences[i].1 * inv * m; 
+            }
+            None => {
+                return None
+            }
+        }
+    }
+    return Some(sum % product);
+}
