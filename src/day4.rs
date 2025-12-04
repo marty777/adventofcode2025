02555aa12037c4e70ec0206ef4bbfbcef156a7af
Marty Thorne <marty@michetti.com>
2025-12-04 11:26:41 -0700

    Minor refactoring of day 4 to iterate over fewer items as rolls are removed

--- src/day4.rs
+++ src/day4.rs
@@ -3,52 +3,39 @@ use crate::util;
 
 /// Print the solutions to day 4 for the given input `lines`
 pub fn run(lines:&Vec<String>) {
-    let part1;
+    let mut part1 = 0;
+    let mut part2 = 0;
     // Load the grid as a DefaultHashMap
     let (grid, width, height) = util::read_grid_map(lines, '.').unwrap();
-    let mut removed_rolls:HashSet<util::Vec2> = HashSet::new();
-    // Iterate each non-default position, counting neighbors of each roll and 
-    // marking them as removed if removable.
-    for pos in grid.keys() {
-        let mut neighbor_count = 0;
-        for dir in util::adjacent8() {
-            let neighbor = *pos + dir;
-            if neighbor.in_bounds(width, height) 
-                && *grid.get(&neighbor) == '@' {
-                neighbor_count += 1;
-            }
-        }
-        if neighbor_count < 4 {
-            removed_rolls.insert(pos.clone());
-        }
-    }
-    part1 = removed_rolls.len();
-    // Continue to iterate over rolls, marking newly removable ones as 
-    // removed. Stop when no more new removable rolls are found.
+    // Get all initial roll positions in the grid
+    let mut remaining_rolls:HashSet<&util::Vec2> = HashSet::from_iter(grid.keys().into_iter());
+    // In a loop, iterate over rolls to find all rolls that can currently be 
+    // removed. If no rolls are marked as removable, exit the loop. Otherwise,
+    // remove the marked rolls and continue with the next pass, recording the 
+    // number of removed items for the part 1 and part 2 answers.
     loop {
-        let mut removed = 0;
-        for pos in grid.keys() {
-            if removed_rolls.contains(pos) {
-                continue;
-            }
+        let mut removed_in_loop = HashSet::new();
+        for pos in remaining_rolls.iter() {
             let mut neighbor_count = 0;
             for dir in util::adjacent8() {
-                let neighbor = *pos + dir;
+                let neighbor = **pos + dir;
                 // Count the neighbor if it's a roll and it hasn't been 
                 // previously removed
                 if neighbor.in_bounds(width, height) 
-                    && *grid.get(&neighbor) == '@' 
-                    && !removed_rolls.contains(&neighbor) {
+                    && remaining_rolls.contains(&neighbor) {
                     neighbor_count += 1;
                 }
             }
             if neighbor_count < 4 {
-                removed += 1;
-                removed_rolls.insert(pos.clone());
+                removed_in_loop.insert((*pos).clone());
             }
         }
-        if removed == 0 { break; }
+        if part1 == 0 { part1 = removed_in_loop.len(); }
+        part2 += removed_in_loop.len();
+        // remove marked rolls from remaining rolls
+        remaining_rolls.retain(|&p| !removed_in_loop.contains(p));
+        if removed_in_loop.len() == 0 { break; }
     }
     println!("Part 1: {}", part1);
-    println!("Part 2: {}", removed_rolls.len());
+    println!("Part 2: {}", part2);
 }
